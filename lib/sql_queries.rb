1e9b05a4e3f908f85336b78fb142bf3985cbb45e
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 02:26:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,5 +46,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  ""
+  "select projects.category, sum(pledges.amount) from projects
+  inner join pledges on projects.id = pledges.project_id 
+  where projects.category = 'books'
+  group by projects.category;"
 end
