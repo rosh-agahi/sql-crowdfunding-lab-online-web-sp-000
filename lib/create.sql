cd92a9b376758f6afbe160fa96a2c165dbc8609d
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 01:49:56 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,20 +1,9 @@
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
-  title text,
+  title TEXT,
   category text,
   funding_goal integer,
   start_date text,
   end_date text);
-  
-CREATE TABLE users (
-  id integer primary key,
-  name text,
-  age integer);
-  
-CREATE TABLE pledges (
-  id integer primary key,
-  amount integer,
-  user_id integer,
-  project_id integer);
   
   
