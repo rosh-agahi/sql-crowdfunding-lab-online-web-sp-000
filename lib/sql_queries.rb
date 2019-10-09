30a444df22acc2632275562c583f1b3f2ca90742
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 02:15:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "select users.name, users.age, sum(pledges.amount), from users 
   inner join pledges 
-  on user.id = pledges.user_id 
+  on users.id = pledges.user_id 
   group by users.name 
   order by users.name;"
 end
