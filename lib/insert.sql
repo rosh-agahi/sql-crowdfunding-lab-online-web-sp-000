ac811d9ad89af3d4570418f71af4b4d7b3732060
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 02:00:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,3 +10,25 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Proj10", "Health", 100000, "9/1/18", "9/30/19");
 
 INSERT INTO users (name, age) VALUES ("Henry", 27);
+INSERT INTO users (name, age) VALUES ("Abigail", 19);
+INSERT INTO users (name, age) VALUES ("John", 31);
+INSERT INTO users (name, age) VALUES ("Matt", 25);
+INSERT INTO users (name, age) VALUES ("Josh", 25);
+INSERT INTO users (name, age) VALUES ("Phil", 60);
+INSERT INTO users (name, age) VALUES ("Dana", 45);
+INSERT INTO users (name, age) VALUES ("Melissa", 24);
+INSERT INTO users (name, age) VALUES ("Spencer", 32);
+INSERT INTO users (name, age) VALUES ("Greg", 29);
+INSERT INTO users (name, age) VALUES ("David", 35);
+INSERT INTO users (name, age) VALUES ("Helen", 18);
+INSERT INTO users (name, age) VALUES ("Angie", 57);
+INSERT INTO users (name, age) VALUES ("Bobby", 90);
+INSERT INTO users (name, age) VALUES ("Mandy", 43);
+INSERT INTO users (name, age) VALUES ("Toby", 34);
+INSERT INTO users (name, age) VALUES ("Hannah", 23);
+INSERT INTO users (name, age) VALUES ("Bryce", 27);
+INSERT INTO users (name, age) VALUES ("Colt", 27);
+INSERT INTO users (name, age) VALUES ("Zach", 28);
+
+INSERT INTO pledges (name, age) VALUES ("Henry", 27);
+
