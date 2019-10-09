e1c553bfbf002bec7ada34eaa783c839a4b65bfc
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 02:21:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "select users.name and sum(pledges.amount) from users
+  "select users.name, sum(pledges.amount) from users
   inner join pledges
   on users.id = pledges.user_id 
   group by users.name
